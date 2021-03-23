1589d93ac7c8eaf5bbc1edab54ddd7238016832b
NikolaiDokken <nikolaidokken@live.no>
2021-03-23 19:43:43 +0100

    Updated to material UI to improve responsiveness

--- src/App.js
+++ src/App.js
@@ -1,171 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect, useState, useCallback } from "react";
-import CocktailService from "./services/CocktailService";
-import Card from "./components/Card";
+import React from "react";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-const searchTypeEnum = { drink: 1, ingredient: 2 };
+import Home from "./views/Home";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: { main: "#fc0303" },
+		secondary: { main: "#ffffff" },
+		text: { primary: "#ffffff" },
+	},
+	typography: {
+		allVariants: {
+			color: "#ffffff",
+		},
+	},
+});
 
 export default function App() {
-    const [drinks, setDrinks] = useState([]);
-    const [searchQuery, setSearchQuery] = useState("");
-    const [searchType, setSearchType] = useState(searchTypeEnum.drink);
-
-    const handleSearch = useCallback(
-        (event) => {
-            if (searchQuery.length > 0 && event.keyCode === 13) {
-                if (document.getElementById("drinkSearchType").checked) {
-                    CocktailService.getCocktailByName(searchQuery).then(
-                        (response) => {
-                            if (response.data.drinks) {
-                                setDrinks(response.data.drinks);
-                            } else {
-                                setDrinks([]);
-                            }
-                        }
-                    );
-                }
-                if (document.getElementById("ingredientSearchType").checked) {
-                    CocktailService.getCocktailByIngredient(searchQuery).then(
-                        (response) => {
-                            if (response.data.drinks) {
-                                setDrinks(response.data.drinks);
-                            } else {
-                                setDrinks([]);
-                            }
-                        }
-                    );
-                }
-            }
-        },
-        [searchQuery]
-    );
-
-    useEffect(() => {
-        CocktailService.getRandomCocktail().then((response) => {
-            setDrinks(response.data.drinks);
-        });
-    }, []);
-
-    useEffect(() => {
-        document
-            .getElementById("searchbar")
-            .addEventListener("keydown", handleSearch, false);
-        return () => {
-            document
-                .getElementById("searchbar")
-                .removeEventListener("keydown", handleSearch, false);
-        };
-    }, [handleSearch]);
-
-    const searchTypeToggle = (event) => {
-        if (event.target.value === "ingredient") {
-            setSearchType(searchTypeEnum.ingredient);
-        } else if (event.target.value) {
-            setSearchType(searchTypeEnum.drink);
-        }
-    };
-
-    return (
-        <div style={styles.app}>
-            <div style={styles.header}>
-                <img
-                    src={logo}
-                    style={styles.appLogo}
-                    className="App-logo"
-                    alt="logo"
-                />
-                The Cocktail Encyclopedia
-            </div>
-            <div style={styles.searcTypeSelector} onChange={searchTypeToggle}>
-                <div style={{ marginRight: 8, fontSize: 16 }}>Search for:</div>
-                <input
-                    type="radio"
-                    id="drinkSearchType"
-                    name="searchType"
-                    value="drink"
-                    defaultChecked
-                />{" "}
-                Drink
-                <input
-                    type="radio"
-                    id="ingredientSearchType"
-                    name="searchType"
-                    value="ingredient"
-                    style={{ marginLeft: 8 }}
-                />{" "}
-                Ingredient
-            </div>
-            <input
-                id={"searchbar"}
-                style={styles.searchBar}
-                value={searchQuery}
-                placeholder={
-                    searchType === searchTypeEnum.drink
-                        ? "Tequila Sunrise"
-                        : "Grenadine"
-                }
-                onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            {searchQuery.length === 0 ? <div>Have you tried...</div> : ""}
-            <div style={styles.cardContainer}>
-                {drinks.map((drink, index) => (
-                    <Card drink={drink} key={index} />
-                ))}
-            </div>
-            {drinks.length === 0 ? <div>No search results</div> : ""}
-        </div>
-    );
+	return (
+		<ThemeProvider theme={theme}>
+			<Home />
+		</ThemeProvider>
+	);
 }
-
-const styles = {
-    app: {
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        fontSize: "calc(10px + 2vmin)",
-        color: "white",
-        justifyContent: "center",
-    },
-    header: {
-        display: "flex",
-        flexDirection: "row",
-        alignSelf: "stretch",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 8,
-    },
-    appLogo: {
-        height: "9vmin",
-        pointerEvents: "none",
-        marginRight: 16,
-    },
-    searcTypeSelector: {
-        fontSize: 14,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    searchBar: {
-        borderRadius: 8,
-        border: "1px solid white",
-        // borderBottom: "4px solid white",
-        height: 32,
-        width: 300,
-        backgroundColor: "inherit",
-        marginTop: 16,
-        fontSize: 20,
-        color: "white",
-        outline: "none",
-        marginBottom: 16,
-    },
-    cardContainer: {
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-evenly",
-        padding: "0 16px 0 16px",
-    },
-};
---
+++ src/views/Home.js
@@ -0,0 +1,173 @@
+import logo from "../logo.svg";
+import "../App.css";
+import { useEffect, useState } from "react";
+import CocktailService from "../services/CocktailService";
+import Card from "../components/Card";
+import {
+	TextField,
+	Typography,
+	InputAdornment,
+	IconButton,
+	Grid,
+	Hidden,
+	Container,
+} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+
+export default function Home() {
+	const [drinks, setDrinks] = useState([]);
+	const [searchQuery, setSearchQuery] = useState("");
+	const [hasSearched, setHasSearched] = useState(false);
+
+	const handleSearch = () => {
+		if (searchQuery.length > 0) {
+			CocktailService.getCocktailByName(searchQuery).then((response) => {
+				if (response.data.drinks) {
+					setDrinks(response.data.drinks);
+				} else {
+					setDrinks([]);
+				}
+			});
+			setHasSearched(true);
+		} else {
+			setHasSearched(false);
+		}
+		/*
+            CocktailService.getCocktailByIngredient(searchQuery).then(
+                (response) => {
+                    if (response.data.drinks) {
+                        setDrinks(response.data.drinks);
+                    } else {
+                        setDrinks([]);
+                    }
+                }
+            );
+        */
+	};
+
+	useEffect(() => {
+		if (!hasSearched) {
+			CocktailService.getRandomCocktail().then((response) => {
+				setDrinks(response.data.drinks);
+			});
+		}
+	}, [hasSearched]);
+
+	const handleChangeSearch = (input) => {
+		setSearchQuery(input.target.value);
+	};
+
+	return (
+		<div style={styles.container}>
+			<Grid
+				container
+				justify="center"
+				alignItems="center"
+				style={{ marginTop: 60 }}
+			>
+				<Hidden smDown>
+					<Grid item>
+						<img
+							src={logo}
+							style={styles.appLogo}
+							className="App-logo"
+							alt="logo"
+						/>
+					</Grid>
+				</Hidden>
+				<Grid item>
+					<Typography variant="h2" align="center">
+						The Cocktail Encyclopedia
+					</Typography>
+				</Grid>
+			</Grid>
+			<TextField
+				value={searchQuery}
+				onChange={handleChangeSearch}
+				placeholder="Tequila sunrise, lime, tonic"
+				color="primary"
+				variant="outlined"
+				InputProps={{
+					endAdornment: (
+						<InputAdornment position="end">
+							<IconButton onClick={handleSearch}>
+								<SearchIcon />
+							</IconButton>
+						</InputAdornment>
+					),
+				}}
+				style={{ marginTop: !hasSearched ? 100 : 20 }}
+			/>
+			{!hasSearched ? (
+				<Typography variant="h5" style={{ marginTop: 40 }}>
+					Have you tried...
+				</Typography>
+			) : (
+				""
+			)}
+			<Container>
+				<Grid
+					container
+					spacing={2}
+					justify={hasSearched ? "flex-start" : "center"}
+				>
+					{drinks.map((drink, index) => (
+						<Grid item xs={12} sm={6} md={4} lg={3}>
+							<Card drink={drink} key={index} />
+						</Grid>
+					))}
+				</Grid>
+			</Container>
+			{drinks.length === 0 ? (
+				<Typography>No search results</Typography>
+			) : (
+				""
+			)}
+		</div>
+	);
+}
+
+const styles = {
+	container: {
+		minHeight: "100vh",
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	},
+	appLogo: {
+		height: "9vmin",
+		pointerEvents: "none",
+		marginRight: 16,
+	},
+	searcTypeSelector: {
+		fontSize: 14,
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	searchContainer: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	searchBar: {
+		borderRadius: 8,
+		border: "1px solid white",
+		height: 32,
+		width: 300,
+		backgroundColor: "inherit",
+		marginTop: 16,
+		fontSize: 20,
+		color: "white",
+		outline: "none",
+		marginBottom: 16,
+	},
+	cardContainer: {
+		display: "flex",
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "space-evenly",
+		padding: "0 16px 0 16px",
+	},
+};
