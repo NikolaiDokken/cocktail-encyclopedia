9b030a7dfdc2ae4743a293bfb390838861dd9962
NikolaiDokken <nikolaidokken@live.no>
2026-04-07 23:26:47 +0200

    Revert "UI changes and refactoring + name change"
    This reverts commit e303d1e850cba76bac3a32b05e4254ea5f270476.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-import Cocktails from "./views/Cocktails";
-import Layout from "./components/Layout";
+import Home from "./views/Home";
 
-const theme = createTheme({
-    palette: {
-        primary: { main: "#F9F9F9" },
-        secondary: { main: "#E0DBD4" },
-        text: { primary: "#F9F9F9", secondary: "#636060" },
-        background: { default: "#1B1917", paper: "#313030" },
-    },
+const theme = createMuiTheme({
+	palette: {
+		primary: { main: "#fc0303" },
+		secondary: { main: "#ffffff" },
+		text: { primary: "#000000" },
+	},
 });
 
 export default function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Layout>
-                <Cocktails />
-            </Layout>
-        </ThemeProvider>
-    );
+	return (
+		<ThemeProvider theme={theme}>
+			<Home />
+		</ThemeProvider>
+	);
 }
